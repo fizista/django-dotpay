7c1110cee5a0b28595722f6b317cb9453b6a01c3
Wojciech Banaś <fizista@gmail.com>
2015-04-11 11:37:15 +0200

    Update admin interface.

--- dotpay/payment/admin.py
+++ dotpay/payment/admin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from dotpay.payment.models import DotRequest,DotResponse
+from dotpay.payment.models import DotRequest, DotResponse
 from dotpay.widget import ModelLinkAdminFields
+
 
 class DotResponseInline(admin.StackedInline):
     model = DotResponse
@@ -10,31 +11,36 @@ class DotResponseInline(admin.StackedInline):
 
     def has_add_permission(self, request, obj=None):
         return False
+
     def has_delete_permission(self, request, obj=None):
         return False
 
 
-class DotResponseAdmin(ModelLinkAdminFields,admin.ModelAdmin):
+class DotResponseAdmin(ModelLinkAdminFields, admin.ModelAdmin):
     modellink = ('request',)
-    list_filter = ['t_date','status','t_status','request__added']
-    search_fields = ['description']
-    list_display = ('__unicode__', 't_date', 't_status','amount','orginal_amount')
+    list_filter = ['t_date', 'status', 't_status', 'request__added']
+    search_fields = ['description', 't_id']
+    list_display = ('__unicode__', 't_date', 't_status', 't_id',
+                    'amount', 'orginal_amount')
 
     def has_add_permission(self, request, obj=None):
         return False
+
     def has_delete_permission(self, request, obj=None):
         return False
-    
+
+
 class DotRequestAdmin(admin.ModelAdmin):
-    inlines = [DotResponseInline,]
-    search_fields = ['opis','email']
+    inlines = [DotResponseInline, ]
+    search_fields = ['opis', 'email']
     list_display = ('opis', 'email', 'added')
-    list_filter = ['added',]
-    
+    list_filter = ['added', ]
+
     def has_add_permission(self, request, obj=None):
         return False
+
     def has_delete_permission(self, request, obj=None):
         return False
-    
+
 admin.site.register(DotRequest, DotRequestAdmin)
-admin.site.register(DotResponse,DotResponseAdmin)
+admin.site.register(DotResponse, DotResponseAdmin)
